6c0bea47294fe97e71c71e1c95ba35de84855276
Anushk Sanghvi <anushksanghvi@gmail.com>
2024-09-24 12:40:41 +0530

    mul function added

--- Calculator.java
+++ Calculator.java
@@ -5,9 +5,13 @@ public class Calculator{
     public static int sub(int a, int b){
         return (a-b);
     }
+    public static int mul(int a, int b){
+        return (a*b);
+    }
 
     public static void main(String[] args) {
         System.out.println(add(2,3));
         System.out.println(sub(2,3));
+        System.out.println(mul(2,3));
     }
 }
